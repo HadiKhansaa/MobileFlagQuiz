640a0ed9d9ef0d9bcb0a0002cb3d4cf1141e106c
hadi <hadi.kansa.950@gmail.com>
2023-05-07 02:55:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/flagquiz/EndScreen.java
@@ -0,0 +1,33 @@
+package com.example.flagquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class EndScreen extends AppCompatActivity {
+
+    TextView result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_screen);
+
+        Intent intent = getIntent();
+        int correctAnswers = intent.getIntExtra("correctAnswers", 0);
+        result = findViewById(R.id.result);
+
+        result.setText("Your final result is " + correctAnswers + " out of 10 correct answers ("+ ((double)correctAnswers/10)*100 + "%)");
+
+    }
+
+    public void resetGame(View v)
+    {
+        Intent i = new Intent(this, FlagQuiz.class);
+        i.putExtra("nbOfChoices", FlagQuiz.nbOfOptions);
+        startActivity(i);
+    }
+
+}
---
+++ app/src/main/java/com/example/flagquiz/SetUp.java
@@ -0,0 +1,65 @@
+package com.example.flagquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.io.IOException;
+
+public class SetUp extends AppCompatActivity {
+
+    private Spinner choiceSpinner;
+    private Button button;
+    private Intent intent;
+    private int nbOfOptions = 0;
+
+    public SetUp() {
+    }
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_up);
+
+        choiceSpinner = findViewById(R.id.choiceSpinner);
+        button = findViewById(R.id.startQuiz);
+        choiceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String choice = (String) adapterView.getItemAtPosition(i);
+
+                if(choice.equals("3 choices")){
+                    nbOfOptions = 3;
+                }
+                else if (choice.equals("6 choices")){
+                    nbOfOptions = 6;
+                }
+                else{
+                    nbOfOptions = 9;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(SetUp.this, FlagQuiz.class);
+                intent.putExtra("nbOfChoices", nbOfOptions);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
